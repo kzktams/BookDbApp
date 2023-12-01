810d22759c44483a9df1987d971f2bfbb24cdb03
kzktams <kzktams@gmail.com>
2023-12-01 22:59:34 +0100

    d

---
+++ R9IOPN_HFT_2023241.Test/BookLogicTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using R9IOPN_HFT_2023241.Logic;
+
+namespace R9IOPN_HFT_2023241.Test
+{
+    [TestFixture]
+    public class BookLogicTest
+    {
+
+        BookLogic logic;
+
+        [SetUp]
+        public void Init()
+        {
+
+        }
+    }
+}
--- R9IOPN_HFT_2023241.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R9IOPN_HFT_2023241.Test
-{
-    public class Class1
-    {
-    }
-}
