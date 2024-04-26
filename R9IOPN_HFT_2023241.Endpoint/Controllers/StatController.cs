776f493180a47e726557c865f3f0ea280abc4add
kzktams <kzktams@gmail.com>
2024-04-26 23:04:04 +0200

    problems

--- R9IOPN_HFT_2023241.Endpoint/Controllers/StatController.cs
+++ R9IOPN_HFT_2023241.Endpoint/Controllers/StatController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
+using R9IOPN_HFT_2023241.Endpoint.Services;
 using R9IOPN_HFT_2023241.Logic;
 using R9IOPN_HFT_2023241.Models;
 
@@ -13,31 +16,36 @@ namespace R9IOPN_HFT_2023241.Endpoint.Controllers
         IBookLogic bookLogic;
         IAuthorLogic authorLogic;
         IUserLogic userLogic;
-        
+        IHubContext<SignalRHub> hub;
 
-        public StatController(IBookLogic bookLogic, IAuthorLogic authorLogic, IUserLogic userLogic)
+
+        public StatController(IBookLogic bookLogic, IAuthorLogic authorLogic, IUserLogic userLogic, IHubContext<SignalRHub> hub)
         {
             this.bookLogic = bookLogic;
             this.authorLogic = authorLogic;
             this.userLogic = userLogic;
-            
+            this.hub = hub;
         }
         //booklogic
         [HttpGet("{authorId}")]
         public IEnumerable<BookDetail> BooksByAuthor(int authorId)
         {
             return this.bookLogic.GetBooksByAuthor(authorId);
+            this.hub.Clients.All.SendAsync("GotBooksByAuthor", authorId);
         }
         [HttpGet]
         public IEnumerable<BookLoanCount> MostLoanedBooks(int books)
         {
             return this.bookLogic.GetMostLoanedBooks();
+            this.hub.Clients.All.SendAsync("GotMostLoanedBooks", books);
         }
 
         [HttpGet("{genre}")]
         public IEnumerable<BookDetail> BooksByGenre(string genre)
         {
             return this.bookLogic.GetBooksByGenre(genre);
+            this.hub.Clients.All.SendAsync("GotBooksByGenre", genre);
+
         }
 
         [HttpGet("{userId}")]
